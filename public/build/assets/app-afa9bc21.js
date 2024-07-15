139017d4cc173c4df316d8616378fafdcfbd7132
estellarr <kristealtamera26@gmail.com>
2024-07-15 19:13:30 +0800

    internship page redirected to log-in page

--- public/build/assets/app-afa9bc21.js
+++
@@ -1,1 +0,0 @@
-import{e as a,P as o,G as s,V as e,Y as r,w as t}from"./tw-elements.es.min-8ab31ca5.js";a({Modal:o,Datepicker:s,Input:e,Carousel:r,Dropdown:t});
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
+
 export default defineConfig({
     plugins: [
         laravel({
@@ -10,4 +11,10 @@ export default defineConfig({
         }),
         react()
     ],
+    resolve: {
+        alias: {
+          // Assuming @inertiajs/inertia-react is located in node_modules/@inertiajs/inertia-react
+          '@inertiajs/inertia-react': '@inertiajs/inertia-react/dist/index.umd.js',
+        },
+      },
 });
